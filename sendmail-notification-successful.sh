8353010962091b7a942978937970f0aa7ab0939d
mehdi-song <mehdi_song@yahoo.com>
2023-01-30 14:41:06 +0330

    Update sendmail-notification-successful.sh

--- sendmail-notification-successful.sh
+++ sendmail-notification-successful.sh
@@ -9,14 +9,21 @@ renewed successfully your Zimbra certificate!
 Sincerelly yours,
 cron"
 
+RED='\033[0;31m'
+CYAN='\033[0;36m'
+NC='\033[0m' # No Color
+
+
 USAGE="USAGE
     $0 email_address
+    
+    ${CYAN}email_address${NC}       Require.
 
     This simple script will send the email to the specified
     e-mail address via '$sendmail' program."
 
 if [ $# -ne 1 ]; then
-    echo "$USAGE" >&2
+    echo -e "$USAGE" >&2
     exit 1
 else
 echo "Subject: $subject
