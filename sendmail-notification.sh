2b69963d8e653428fe01b9db607b1fd4da392c7f
mehdi-song <mehdi_song@yahoo.com>
2023-01-30 14:35:22 +0330

    Update sendmail-notification.sh

--- sendmail-notification.sh
+++ sendmail-notification.sh
@@ -9,25 +9,33 @@ will try to obtain and install new zimbra certificate in $1 day(s).
 Sincerelly yours,
 cron"
 
+RED='\033[0;31m'
+CYAN='\033[0;36m'
+NC='\033[0m' # No Color
+
+
 USAGE="USAGE
-    $0 days email_address
+    $0 day(s) email_address
+    
+    ${CYAN}day(s)${NC}              Require.Must be a natural number
+    ${CYAN}email_address${NC}       Require.
 
     This simple script will send the email to the specified 
     e-mail address via '$sendmail' program."
 
 [ $# -ne 2 ] && {
-    echo "$USAGE" >&2
+    echo -e "$USAGE" >&2
     exit 1
 }
 
 [[ "$1" =~ ^[0-9]+$ ]] || {
-    echo "The first arg '$1' must be a natural number" >&2
+    echo -e "${RED}The first arg '$1' must be a natural number${NC}" >&2
     exit 1
 }
 
 [ $# -eq 1 ] && {
     if [ "$1" == "-h" -o "$1" == "--help" -o "$1" == "help" ]; then
-    echo "$USAGE" >&2
+    echo -e "$USAGE" >&2
     exit 1
     fi
 }
